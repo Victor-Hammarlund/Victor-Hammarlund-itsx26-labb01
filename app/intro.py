52d762c0c598b2344b664745855683a6d4f4f621
Victor <lars.hammarlund@student.chasacademy.se>
2026-08-30 14:01:01 +0200

    Fix welcome message formatting in intro.py
    Removed welcome message line break in intro.py.

--- app/intro.py
+++ app/intro.py
@@ -4,7 +4,7 @@ NAME = ""
 letter = ""
 print("Vem är du?")
 NAME = input()
-print(f"\nHej ITSX26!\nJag studerar IT- och cybersäkerhet.\nMitt namn är {NAME}.\n Välkommen!\n")
+print(f"\nHej ITSX26!\nJag studerar IT- och cybersäkerhet.\nMitt namn är {NAME}.")
 print("\n--program klart--\n")
 
 
