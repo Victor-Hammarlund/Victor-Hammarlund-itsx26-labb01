8b489eb8e07c486025fd5003a3a434de6adc20cd
Victor Hammarlund <lars.hammarlund@student.chasacademy.se>
2026-09-18 14:26:20 +0200

    network check script done

--- scripts/secure_network_check.sh
+++ scripts/secure_network_check.sh
@@ -2,54 +2,146 @@
 
 
 #Variables
+EVIDENCE="../docs/secure_network_check_log_extended.log"
 LOG_FILE="../docs/secure_network_check.log"
 TARGET_DOMAIN_ARR=("example.com" "google.com" "chasacademy.instructure.com")
 TARGET_PORT=8080
-TEST_SERVER="python3 -m http.server $TARGET_PORT --bind 127.0.0.1"
 PID_TEST_SERVER=0
-declare -A NINF=()
+declare -a TOOLS=("curl" "ss" "getent" "pgrep" "pkill" "ip" "date" "tee" "python3" "sed")
 
+SUCCESS=0
+FAILED=0
 #-----------------------------------------------------------#
 
 # Function to format log messages with timestamp
 log_message() {
-    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
-    STATUS=$1
-    MESSAGE=$2
+    local TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    local STATUS=$1
+    local MESSAGE=$2
     echo "[$TIMESTAMP] [$STATUS] $MESSAGE" | tee -a "$LOG_FILE"
+	
+	#Case-switch logic for summery of statuses
+	case "$STATUS" in
+
+	OK)
+	((SUCCESS++))
+	;;
+
+	FAIL)
+	((FAILED++))
+	;;
+
+	esac
+	
+
+}	
+	
+
+#-----------------------------------------------------------#
+
+start()
+{	
+	#Start logic to determine if to wipe logs
+	while true;
+	do
+		echo $'IF YOU CONTINUE THE PREVIOUS LOGS WILL BE WIPED!'
+		echo $'\nCONTINUE?  y/n'
+		# User input determines if to continue or not
+		read -r input
+
+		if [[ "$input" =~ ^[yY]+$ ]]; then
+			clear
+			echo "STARTING NETWORK CHECK ..."
+			sleep 2
+			#Program coninues normaly.
+			break
+		elif [[ "$input" =~ ^[nN]+$ ]]; then
+			clear
+			echo "EXITING ..."
+			sleep 2
+			exit 0; #Exiting normally, no error.
+		else
+			#User input wasnt valid so loop starts over
+			clear
+			echo "input invalid INPUT: $input"
+			sleep 2
+			clear
+		fi
+	done
 }
 
 #-----------------------------------------------------------#
 
+#Determine if system has the required commands installed
+validate_command()
+{
+	
+
+	declare -a FAILED_TOOLS=()		
+
+	#Appends missing command to array
+	for command in "${TOOLS[@]}";do
+		if ! command -v "$command" &> /dev/null; then
+			FAILED_TOOLS+=("$command")
+		fi
+	done
+
+	#If missing atleast one command the program is interupted with exit code 1
+	if [ ${#FAILED_TOOLS[@]} -ne 0 ]; then
+		log_message FAIL "The following required commands are missing: ${FAILED_TOOLS[*]}"
+		exit 1
+	else 
+		log_message OK "All required commands are available."
+	fi
+}
+
+#-----------------------------------------------------------#
+
+#Stops test service, and sanitzes output
 cleanup(){
+	#Replace regexmatch with replacement string
+	sed -Ei 's/127(\.[0-9]{1,3}\.){2}[1]{1,3}/[LOCALHOST]/g' $EVIDENCE
+	sed -Ei 's/([0-9]{1,3}\.){3}[0-9]{1,3}/[REDACTED]/g' $EVIDENCE
+	
+	#Closes test server, if running
     log_message INFO "Checking if test server is running"
-    if  pgrep -f "$TEST_SERVER" > /dev/null; then
+
+	
+	if [ -z "$PID_TEST_SERVER" ]; then
+		log_message FAIL "Cleanup failure, no server running or could not stop service"
+    elif [ $PID_TEST_SERVER -gt 0 ] > /dev/null; then
         log_message INFO "Shutting down test server"
-        pkill -f "python3 -m http.server $TARGET_PORT"
+        kill "$PID_TEST_SERVER"
         log_message OK "Test server stopped"
-    else
-        log_message OK "No test server running";
-    fi
+    #Catch unexpected errors
+	else	
+		log_message WARN "Unexpected error during service cleanup"
+	fi
 
-
+	summery
+	
 }
 #-----------------------------------------------------------#
 
 startTestServer()
 {
-	log_message INFO Starting Test server
+
+	echo "#////LOCAL TEST SERVER////" >> "$EVIDENCE"
+	log_message INFO "Starting Test server";
+	#Start a http server to serve http on localhost
 	python3 -m http.server $TARGET_PORT --bind 127.0.0.1 &
-	PID_TEST_SERVER=$(pgrep -f "$TEST_SERVER")
-	
+	#save process id to variable
+	PID_TEST_SERVER=$!
+	jobs
 	sleep 1
 	log_message INFO "fetching HTTP from localhost"
-	echo "#/////////////////////////////////////////////////#"
-	if curl localhost:8080; then
-		 echo "#/////////////////////////////////////////////////#"
-
+	
+	#Is http server serving http from localhost
+	if curl localhost:"$TARGET_PORT" >> "$EVIDENCE" 2>&1; then
 		log_message OK "Succesfully fetch http from localhost"
+		log_message INFO "Test server info at $EVIDENCE"
 	else
-		log_message FAIL "Could not curl towards localhost"
+		log_message FAIL "Could not curl towards localhost:$TARGET_PORT"
 	fi
 }
 
@@ -57,70 +149,121 @@ startTestServer()
 
 dnsCheck()
 {
-    if [ -z "$TARGET_DOMAIN_ARR" ]; then
+	#Were domain names given to test
+    if [ ${#TARGET_DOMAIN_ARR[@]} -eq 0 ]; then
         log_message WARN "No domains provided"
     else
+		# Try to reslove each domain in array
 	    for domain in "${TARGET_DOMAIN_ARR[@]}"
 	    do
 		if getent hosts "$domain" >/dev/null; then
-			log_message OK "$domain is reachable"
+			log_message OK "$domain was resolved successfully"
 		else
-			log_message FAIL "$domain is not reachable"
+			log_message FAIL "$domain could not be resolved"
 		fi
 		done
     fi
 }
 
-directoryExists()
+#-----------------------------------------------------------#
+
+logsExists()
 {
-echo Hello
+	declare -a LOGS=("$LOG_FILE" "$EVIDENCE")
+	for log in "${LOGS[@]}";do
+		if [ -e "$log" ]; then 
+			log_message OK "$log Exists"
+		else
+			log_message INFO "$log doesnt exist, creating directory . . ."
+			touch "$log"
+			log_message OK "$log created"
+		fi
+	done
 }
+
+#-----------------------------------------------------------#
 
 openPorts()
 {
 	log_message INFO "Fetching listening ports"
-	echo "#/////////////////////////////////////////////////#"	
-	ss -tuln 
-	echo "#/////////////////////////////////////////////////#"
-
+	echo "////LISTENING PORTS////" >> "$EVIDENCE"	
+	#Lists currently listening ports
+	ss -tuln | grep -e tcp -e udp | sed -E 's/:([0-9]{1,5})/:[PORT]/g' >> "$EVIDENCE"
+	openPortCount="$(ss -tuln | grep -e tcp -e udp| wc -l)" 
+	log_message INFO "Number of open ports: $openPortCount | More at evidence file"
 }
+
+#-----------------------------------------------------------#
 
 routing()
 {
 	declare -A NINF=()
+	
+
 
 	for iface in /sys/class/net/*; do
+		if [ -r "$iface/carrier" ]; then
        		NINF[$(basename "$iface")]=$(<"$iface/carrier");
+		fi
 	done
 
+	echo "////NETWORK INTERFACES & ROUTE////" >> "$EVIDENCE"
+	ip -br a  >> "$EVIDENCE"
+	echo "" >> "$EVIDENCE"
+	ip route show >> "$EVIDENCE"
+	
 	for i in "${!NINF[@]}"; do
-        	echo "${i}: ${NINF[$i]}";
+			
+			if [ "${NINF[$i]}" -ne 1 ]; then
+			log_message WARN "${i} is not connected";
+			else
+			log_message OK "${i} is connected"
+			fi 
 	done
+ 
 
 
-
-	interfaceCount="$(ip -br a | wc -l)";
-	
-	#if wired/wireless connection up
-		# log connection is wired/wireless
-	#else if
-		# log warning no wired/wireless connection
-	
-	#else 
-		#log OK Connected via (show type of connection)
-		# show address information 
 }
 
+summery()
+{
 
+	echo
+	echo "========== SUMMARY =========="
+	echo "Successful checks : $SUCCESS"
+	echo "Failed checks     : $FAILED"
+	echo "Log file          : $LOG_FILE"
+	echo "Evidence file     : $EVIDENCE"
+	echo "============================="
+
+}
+
+#-----------------------------------------------------------#
 
 main() {
-    log_message INFO "Starting secure network check"
-   
-    dnsCheck
-    startTestServer
-    cleanup
-    openPorts
-    log_message INFO "Secure network check completed"
+	# Cleanup is done at end of program or if program is terminated early 
+	trap cleanup EXIT INT TERM
+	# Is user rooted?
+	
+
+		# Prepare network check
+		start
+		clear
+		logsExists
+		# Wipe logfiles
+		> $LOG_FILE
+		> $EVIDENCE
+
+		# Start Network check
+		log_message INFO "Starting secure network check"
+		validate_command
+		routing
+    	dnsCheck
+    	startTestServer
+    	openPorts
+    	log_message INFO "Secure network check completed"
+		
+	
 }
 
 
